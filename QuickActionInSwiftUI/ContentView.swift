a0630bbaef786b71f1c72fe8cc9328575ffa7efa
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-01-04 11:39:00 +0200

    Initial Commit

---
+++ QuickActionInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickActionInSwiftUI
+//
+//  Created by ramil on 04.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuickActionInSwiftUI/QuickActionInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickActionInSwiftUIApp.swift
+//  QuickActionInSwiftUI
+//
+//  Created by ramil on 04.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct QuickActionInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
